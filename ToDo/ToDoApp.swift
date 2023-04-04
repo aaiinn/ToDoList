df4ddeeeef1c75957a5782f14a8153aa89b99b36
Shin ain <s22028@gsm.hs.kr>
2023-04-04 20:00:04 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by 신아인 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
